d40a62370dcb4589e3e51cc45880b0f150cfdd3b
PoisedProto <120425164+PoisedProto@users.noreply.github.com>
2024-12-03 22:04:59 -0700

    1.03

--- software/Lightspeed_Manager.bat
+++ software/Lightspeed_Manager.bat
@@ -3,18 +3,24 @@
 :menu
 cls
 echo ============================
-echo      Lightspeed Manager
+echo   Lightspeed Manager v1.03
 echo This program lets you start and stop Lightspeed Classroom, internally called ClassroomWindows.
 echo ============================
-echo 1. Auto-Block Lightspeed Classroom
+echo 1. Auto-Close Lightspeed Classroom
 echo 2. Launch Lightspeed Classroom
-echo 3. Exit
 echo ============================
-set /p choice=Select an option (1-3): 
+
+:: Warn user if Neutrino is already running.
+tasklist | find /i "neutrino.exe" >nul || tasklist | find /i "nutrino.exe" >nul
+if not errorlevel 1 (
+	echo WARNING: Neutrino is running in the background too! This can cause problems.
+	echo 3. Close Neutrino
+)
+set /p choice=Select an option (number keys): 
 
 if "%choice%"=="1" goto start
 if "%choice%"=="2" goto manualstart
-if "%choice%"=="3" exit
+if "%choice%"=="3" goto manualstopneutrino
 goto menu
 
 :start
@@ -41,6 +47,12 @@ cls
 :: Checks to see if it is already running
 tasklist | find /i "ClassroomWindows.exe" >nul
 if errorlevel 1 (
+	:: Closes Neutrino if it is running
+	tasklist | find /i "neutrino.exe" >nul || tasklist | find /i "nutrino.exe" >nul
+	if not errorlevel 1 (
+		echo Closing Neutrino.
+		goto autostopneutrino
+	)
 	echo Attempting to start "ClassroomWindows.exe"...
 	start "" "C:\Program Files\Lightspeed Systems\Classroom Agent\ClassroomWindows.exe"
 	echo Checking for running process...
@@ -57,3 +69,18 @@ if errorlevel 1 (
 )
 pause
 goto menu
+
+:manualstopneutrino
+:: For the menu
+cls
+taskkill /f /im Neutrino.exe
+taskkill /f /im Nutrino.exe
+pause
+goto menu
+
+:autostopneutrino
+:: For when you start Lightspeed manually
+taskkill /f /im Neutrino.exe
+taskkill /f /im Nutrino.exe
+timeout /t 2 /nobreak >nul
+goto manualstart
